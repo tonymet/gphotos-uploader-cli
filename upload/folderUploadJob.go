4984370335df47072fa7236a11e132bcacf340cd
Anthony Metzidis <anthony.metzidis@gmail.com>
2019-01-28 22:15:02 -0800

    Support 5 concurrent uploads

--- upload/folderUploadJob.go
+++ upload/folderUploadJob.go
@@ -91,7 +91,7 @@ func authenticate(folderUploadJob *FolderUploadJob) (*gphotos.Client, error) {
 }
 
 // Upload uploads folder
-func (folderUploadJob *FolderUploadJob) Upload() error {
+func (folderUploadJob *FolderUploadJob) Upload(fileUploadsChan chan *FileUpload) error {
 	folderAbsolutePath, err := cp.AbsolutePath(folderUploadJob.SourceFolder)
 	if err != nil {
 		return err
@@ -140,7 +140,7 @@ func (folderUploadJob *FolderUploadJob) Upload() error {
 			}
 
 			// finally, add the file upload to the queue
-			QueueFileUpload(fileUpload)
+			fileUploadsChan <- fileUpload
 		}
 
 		return nil
